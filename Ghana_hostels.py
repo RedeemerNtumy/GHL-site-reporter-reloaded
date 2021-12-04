dea60e91d62bedb09307224f6feb6e8e311c0d7d
Redeemer Ntumy <redeemer@Redeemers-MacBook-Air.local>
2021-12-04 16:53:04 +0000

    Testing

---
+++ Ghana_hostels.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+driver = webdriver.Chrome('/usr/local/bin/chromedriver')
+driver.get("https://www.ghanahostels.org/student")
+signup="/html/body/div/div/div/div/div/div[3]/form/button"
+id_input="/html/body/div/div/div/div/div/div[3]/form/div[1]/input"
+password_input="/html/body/div/div/div/div/div/div[3]/form/div[2]/input"
+driver.find_element_by_xpath(id_input).send_keys("10712863")
+driver.find_element_by_xpath(password_input).send_keys("redeemerntumy4019")
+driver.find_element_by_xpath(signup).click()
+no_rooms=driver.find_element(By.XPATH,"/html/body").text
+while no_rooms==driver.find_element(By.XPATH,"/html/body").text:
+    time.sleep(5)
+    print(1)
+
+
