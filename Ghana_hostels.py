820cdc3504204251a9e1ac7d60193e9ebf7af761
Redeemer Ntumy <55901355+RedeemerNtumy@users.noreply.github.com>
2021-12-04 17:35:36 +0000

    Update Ghana_hostels.py

--- Ghana_hostels.py
+++ Ghana_hostels.py
@@ -7,10 +7,12 @@ driver.get("https://www.ghanahostels.org/student")
 signup="/html/body/div/div/div/div/div/div[3]/form/button"
 id_input="/html/body/div/div/div/div/div/div[3]/form/div[1]/input"
 password_input="/html/body/div/div/div/div/div/div[3]/form/div[2]/input"
-driver.find_element(By.XPATH,id_input).send_keys("10712863")
-driver.find_element(By.XPATH,password_input).send_keys("redeemerntumy4019")
+
+driver.find_element(By.XPATH,id_input).send_keys("YOUR ID")
+driver.find_element(By.XPATH,password_input).send_keys("YOUR PASSWORD")
 driver.find_element(By.XPATH,signup).click()
 while True:
-    if "Kindly visit the platform when the portal is opened for reservation / booking." in driver.find_element(By.XPATH,"/html/body").text:   
+    if "Kindly visit the platform when the portal is opened" in driver.find_element(By.XPATH,"/html/body").text:   
         print(1)
+        time.sleep(5)
 
