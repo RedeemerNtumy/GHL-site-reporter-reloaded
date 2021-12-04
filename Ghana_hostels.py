24d00805967656bb1ce077d43e7baeef22abfa2a
Redeemer Ntumy <redeemer@Redeemers-MacBook-Air.local>
2021-12-04 17:03:55 +0000

    Modifications

--- Ghana_hostels.py
+++ Ghana_hostels.py
@@ -7,12 +7,9 @@ driver.get("https://www.ghanahostels.org/student")
 signup="/html/body/div/div/div/div/div/div[3]/form/button"
 id_input="/html/body/div/div/div/div/div/div[3]/form/div[1]/input"
 password_input="/html/body/div/div/div/div/div/div[3]/form/div[2]/input"
-driver.find_element_by_xpath(id_input).send_keys("10712863")
-driver.find_element_by_xpath(password_input).send_keys("redeemerntumy4019")
-driver.find_element_by_xpath(signup).click()
-no_rooms=driver.find_element(By.XPATH,"/html/body").text
-while no_rooms==driver.find_element(By.XPATH,"/html/body").text:
-    time.sleep(5)
-    print(1)
-
-
+driver.find_element(By.XPATH,id_input).send_keys("10712863")
+driver.find_element(By.XPATH,password_input).send_keys("redeemerntumy4019")
+driver.find_element(By.XPATH,signup).click()
+while True:
+    if "Kindly visit the platform when the portal is opened for reservation / booking." in driver.find_element(By.XPATH,"/html/body").text:   
+        print(1)
