032b19bbefcd2c684203382e9e661fc8e1531157
Redeemer Ntumy <redeemer@Redeemers-MacBook-Air.local>
2021-12-05 02:09:30 +0000

    Working.Time to specify the actions

--- Ghana_hostels.py
+++ Ghana_hostels.py
@@ -3,20 +3,30 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 import info
+import config
 import time
-driver = webdriver.Chrome('/usr/local/bin/chromedriver')
+
 count=1
-messages=10
+messages=50
 options=webdriver.ChromeOptions()
-options.add_argument("--user-data-dir=/Library/Application Support/Google/Chrome") #User data direcotry for Chrome. This is for MAC
-options.add_argument("profile-directory=Default")
+options.add_argument(config.CHROME_PROFILE_PATH) 
+driver = webdriver.Chrome('/usr/local/bin/chromedriver',options=options)
+
 driver.get("https://www.ghanahostels.org/student")
+
 signup="/html/body/div/div/div/div/div/div[3]/form/button"
+
 id_input="/html/body/div/div/div/div/div/div[3]/form/div[1]/input"
+
 password_input="/html/body/div/div/div/div/div/div[3]/form/div[2]/input"
+
 my_personal_whatsapp_group_xpath='//*[@id="pane-side"]/div[1]/div/div/div[12]/div/div/div[2]'
-whatsapp="https://web.whatsapp.com"
+
 chatbox='//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[2]'
+
+whatsapp="https://web.whatsapp.com"
+
+
 
 driver.find_element(By.XPATH,id_input).send_keys(info.your_id)
 driver.find_element(By.XPATH,password_input).send_keys(info.your_password)
@@ -28,12 +38,12 @@ while True:
     else:
         driver.find_element_by_tag_name('body').send_keys(Keys.COMMAND + 't')
         driver.get(whatsapp)
-        time.sleep(65)
+        time.sleep(20) # Change time to 20 after first login
         while count <= messages:
             try:
                 group=driver.find_element(By.XPATH,'//span[@title = "{}"]'.format(info.chat))
                 group.click()
-                driver.find_element(By.XPATH,chatbox).send_keys("I am using this group to test my code.This text was actually automated")
+                driver.find_element(By.XPATH,chatbox).send_keys("GHL bot v1.2 Speaking: The Ghana Hostels portal is open ‼️‼️‼️‼️")
                 button=driver.find_element(By.CLASS_NAME,'_4sWnG')
                 button.click()
                 count = count + 1
