4f5d9ed2ce4427952f06de51307e1979cf31515a
Redeemer Ntumy <55901355+RedeemerNtumy@users.noreply.github.com>
2021-12-17 15:51:50 +0000

    Update Ghana_hostels.py

--- Ghana_hostels.py
+++ Ghana_hostels.py
@@ -2,21 +2,17 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
-import info
-import config
+'''
+You do not need to import the info and config files as you can directly input your details here
+'''
 import time
 from playsound import playsound
-import AppKit
+import AppKit # This may not work on windows to play a sound
 import requests
 
+driver = webdriver.Chrome('/usr/local/bin/chromedriver')
 
-count=1
-messages=10
-options=webdriver.ChromeOptions()
-options.add_argument(config.CHROME_PROFILE_PATH) 
-driver = webdriver.Chrome('/usr/local/bin/chromedriver',options=options)
-
-driver.get("https://www.ghanahostels.org/student")
+driver.get("https://ghanahostels.org/student/reserve_room")
 
 signup="/html/body/div/div/div/div/div/div[3]/form/button"
 
@@ -28,48 +24,33 @@ my_personal_whatsapp_group_xpath='//*[@id="pane-side"]/div[1]/div/div/div[12]/di
 
 chatbox='//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[2]'
 
-whatsapp="https://web.whatsapp.com"
 
-
-
-driver.find_element(By.XPATH,id_input).send_keys(info.your_id)
-driver.find_element(By.XPATH,password_input).send_keys(info.your_password)
+driver.find_element(By.XPATH,id_input).send_keys(info.your_id) #Replace info.your_id with a string of your ID
+driver.find_element(By.XPATH,password_input).send_keys(info.yout_password)# Replace info.your_password with a string of your password
 driver.find_element(By.XPATH,signup).click()
+
+
+'''
+Below are some of the network errors that may arise during the process.Check to see if is would be different from Windows
+'''
+dino="No Internet\nTry:\nChecking the network cables, modem and router\nReconnecting to Wi-Fi\nERR_INTERNET_DISCONNECTED\nDino game. A pixelated dinosaur dodges cacti and pterodactyls as it runs across a desolate landscape. When you hear an audio cue, press space to jump over obstacles."
 no_add="This site can’t be reached\nwww.ghanahostels.org’s server IP address could not be found.\nTry:\nChecking the connection\nChecking the proxy, firewall and DNS configuration\nERR_NAME_NOT_RESOLVED\nReload\nDetails"
 reach="This site can’t be reached\nwww.ghanahostels.org’s DNS address could not be found. Diagnosing the problem.\nDNS_PROBE_STARTED\nReload"
 connect_close="This site can’t be reached\nwww.ghanahostels.org unexpectedly closed the connection.\nTry:\nChecking the connection\nChecking the proxy and the firewall\nERR_CONNECTION_CLOSED\nReload\nDetails"
 interrupt="Your connection was interrupted\nA network change was detected.\nERR_NETWORK_CHANGED\nReload"
 connect_off="This site can’t be reached\nwww.ghanahostels.org took too long to respond.\nTry:\nChecking the connection\nChecking the proxy and the firewall\nERR_TIMED_OUT\nReload\nDetails"
-test="MAIN\nDashboard\nSETTINGS\nMy Profile\nLogout\nGhana Hostels Limited\nWelcome, Redeemer Kwame\nDashboard\nUniversity of Ghana Student Homepage\nNotice\nCongratulations !!\nYour profile has been validated.\nKindly visit the platform when the portal is opened for reservation / booking.\nClick here to view profile"
+block="Select a block"
 while True:
     time.sleep(3)
-    if test ==driver.find_element(By.XPATH,"/html/body").text:   
+    driver.find_element(By.XPATH,'/html/body/div[1]/div/div[1]/ul/li[4]/a').click()
+    time.sleep(2)
+    driver.find_element(By.XPATH,'//*[@id="block"]').click()
+    if block ==driver.find_element(By.XPATH,'//*[@id="block"]').text:
         print("Checking....")
-        AppKit.NSBeep()
-        driver.refresh() 
-    elif connect_off==driver.find_element(By.XPATH,"/html/body").text or interrupt==driver.find_element(By.XPATH,"/html/body").text or connect_close==driver.find_element(By.XPATH,"/html/body").text or reach==driver.find_element(By.XPATH,"/html/body").text or no_add==driver.find_element(By.XPATH,"/html/body").text:
         driver.refresh() 
     else:
-        print(driver.find_element(By.XPATH,"/html/body").text)
-        playsound("GhanaHostels.mp4")
-        driver.find_element(By.TAG_NAME,'body').send_keys(Keys.COMMAND + 't')
-        driver.get(whatsapp)
-        playsound("GhanaHostels.mp4")
-        time.sleep(20)
+        playsound("YOUR MP4 THAT WOULD PLAY WHEN THE PORTAL IS OPEN.mp4") #PLACE IN SAME DIRECTORY
         
-        while count <= messages:
-            try:
-                print(info.chat)
-                group=driver.find_element(By.XPATH,f'//span[@title = "{info.chat}"]')
-                group.click()
-                driver.find_element(By.XPATH,chatbox).send_keys("GHL Bot v1.2: THE GHANA HOSTELS PORTAL IS OPEN. *Someone please call me if I am not online so I can book a room* ")
-                button=driver.find_element(By.CLASS_NAME,'_4sWnG')
-                button.click()
-            except Exception as e:
-                print(e)
-            count+=1
-        playsound("GhanaHostels.mp4")
-    driver.find_element(By.TAG_NAME,'body').send_keys(Keys.COMMAND + 't')
-   
+    
    
 
